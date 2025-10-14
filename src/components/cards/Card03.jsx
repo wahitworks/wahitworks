653d31f66c5eb3b15bcaa9121a6fdb4da9d6aa21
underthbluemoon <underthebluemoon0@gmail.com>
2025-10-14 09:33:38 +0900

    251014 CARD01->03 컴포넌트명 수정

--- src/components/cards/Card03.jsx
+++ src/components/cards/Card03.jsx
@@ -1,9 +1,37 @@
 import "./Card03.css";
+import LogoGood from '../commons/LogoGood';
+import LogoModerate from '../commons/LogoModerate';
+import LogoBad from '../commons/LogoBad';
+import LogoVeryBad from '../commons/LogoVeryBad';
 
 function Card03() {
   return (
     <>
-      <h1 className="card03-container">card03</h1>
+      <div className="card01-container">
+        <h1>지금 대기 상태</h1>
+        <div className="card01-result-cotainer">
+          <div className="card01-result-item">
+            <p>미세먼지</p>
+            <p>1023㎍/㎥</p>
+            <LogoGood animated />
+          </div>
+          <div className="card01-result-item">
+            <p>미세먼지</p>
+            <p>1023㎍/㎥</p>
+            <LogoModerate animated />
+          </div>
+          <div className="card01-result-item">
+            <p>미세먼지</p>
+            <p>1023㎍/㎥</p>
+            <LogoBad animated />
+          </div>
+          <div className="card01-result-item">
+            <p>미세먼지</p>
+            <p>1023㎍/㎥</p>
+            <LogoVeryBad animated />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
