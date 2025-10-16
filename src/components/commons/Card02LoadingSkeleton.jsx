0ff2b05bd5e8d45aa9c1bf53f36de625af13aec9
taehoLee950 <inwoo950@gmail.com>
2025-10-16 18:11:25 +0900

    251016 CARD02 스켈레톤 색상 일치하게 변경

--- src/components/commons/Card02LoadingSkeleton.jsx
+++ src/components/commons/Card02LoadingSkeleton.jsx
@@ -1,41 +1,53 @@
 import React from "react";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import "../cards/Card02.css"; // 실제 컴포넌트의 CSS를 재사용하여 레이아웃을 일치시킵니다.
+import "../cards/Card02.css";
 
 const Card02LoadingSkeleton = () => {
   return (
-    <div className="card02-container">
-      <h2 style={{ marginBottom: '5px' }}>
-        <Skeleton width="60%" height={28} />
-      </h2>
-      <p className="card02-measure-time" style={{ marginBottom: '10px' }}>
-        <Skeleton width="40%" height={12} />
-      </p>
-      <div className="day-navigation">
-        <Skeleton circle width={35} height={35} />
-        <div className="card02-date-flex-container" style={{ alignItems: 'center' }}>
-          <Skeleton width={50} height={24} />
-          <Skeleton width={90} height={16} />
-        </div>
-        <Skeleton circle width={35} height={35} />
-      </div>
-      <div className="forecasts-grid">
-        {[...Array(3)].map((_, i) => (
-          <div className="forecast-item" key={i}>
-            <h4>
-              <Skeleton width="80%" />
-            </h4>
-            <div className="forecast-img-wrapper">
-              <Skeleton circle width={60} height={60} />
-            </div>
-            <p>
-              <Skeleton width="50%" />
-            </p>
+    <SkeletonTheme baseColor="#e6e9ecff" highlightColor="#f8f9fa">
+      <div
+        className="card02-container"
+        style={{ backgroundColor: "#e6e9ecff", border: "none" }}
+      >
+        <h2 style={{ marginBottom: "5px" }}>
+          <Skeleton height={28} />
+        </h2>
+        <p className="card02-measure-time" style={{ marginBottom: "10px" }}>
+          <Skeleton height={12} />
+        </p>
+        <div className="day-navigation">
+          <Skeleton circle width={35} height={35} />
+          <div
+            className="card02-date-flex-container"
+            style={{ alignItems: "center", gap: "4px" }}
+          >
+            <Skeleton height={24} width={50} />
+            <Skeleton height={16} width={90} />
           </div>
-        ))}
+          <Skeleton circle width={35} height={35} />
+        </div>
+        <div className="forecasts-grid">
+          {[...Array(3)].map((_, i) => (
+            <div
+              className="forecast-item"
+              key={i}
+              style={{ backgroundColor: "transparent", border: "none" }}
+            >
+              <h4>
+                <Skeleton />
+              </h4>
+              <div className="forecast-img-wrapper">
+                <Skeleton circle width={60} height={60} />
+              </div>
+              <p>
+                <Skeleton />
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </SkeletonTheme>
   );
 };
 
